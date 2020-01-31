318f64cfd6e03caee14d34ae662c35f6af86b08a
Yashu7 <janmalawski2@gmail.com>
2020-02-01 00:06:46 +0100

    Generate new jobs.
    Signed-off-by: Yashu7 <janmalawski2@gmail.com>

--- Assets/Scripts/GenerateJob.cs
+++ Assets/Scripts/GenerateJob.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class GenerateJob : MonoBehaviour
 {
     public GameObject part;
+    public GameObject firstJob;
+    public GameObject secondJob;
+    public GameObject thirdJob;
+
+    public List<GameObject> createdJobs = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(part, new Vector3(1 , 1, 0), Quaternion.identity);
-        Instantiate(part, new Vector3(5, 1, 0), Quaternion.identity);
-        Instantiate(part, new Vector3(-5, 1, 0), Quaternion.identity);
+
+
+        thirdJob = Instantiate(part, new Vector3(5, 1, 0), Quaternion.identity);
+        secondJob = Instantiate(part, new Vector3(1, 1, 0), Quaternion.identity);
+        firstJob = Instantiate(part, new Vector3(-5, 1, 0), Quaternion.identity);
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (secondJob == null)
+        {
+            secondJob = Instantiate(part, new Vector3(1, 1, 0), Quaternion.identity);
+           
+        }
+        if (firstJob == null)
+        {
+            firstJob = Instantiate(part, new Vector3(-5, 1, 0), Quaternion.identity);
+
+        }
+        if (thirdJob == null)
+        {
+            thirdJob = Instantiate(part, new Vector3(5, 1, 0), Quaternion.identity);
+
+        }
     }
 }
