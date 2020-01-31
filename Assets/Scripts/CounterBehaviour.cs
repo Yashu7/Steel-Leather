e8a280e2cb555376f9fcc79ff1355322725fb716
mmalawski <mieszkomalawski@cdprojektred.com>
2020-02-01 00:08:38 +0100

    timer + remove part on timeout

---
+++ Assets/Scripts/CounterBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterBehaviour : MonoBehaviour
+{
+    public int currentTimeInSec;
+    public int minStartingTimeInSec = 5;
+    public int maxStartingTimeInSec = 15;
+    public GameObject displayCounter;
+    public GenerateJob job;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Creating counter");
+        currentTimeInSec = Random.Range(minStartingTimeInSec, maxStartingTimeInSec);
+
+        InvokeRepeating("OutputTime", 1.0f, 1.0f);
+    }
+
+    public void AttachJob(GenerateJob jobToAttach) {
+        job = jobToAttach;
+    }
+
+    void OutputTime() {
+     if(currentTimeInSec < 1) {
+         CancelInvoke();
+         Debug.Log("Dropping part");
+         job.removePart();
+         return;
+     }
+     currentTimeInSec--;
+ }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayCounter = GameObject.Find("Counter1");
+        displayCounter.GetComponent<Text>().text = currentTimeInSec.ToString();
+    }
+}
--- Assets/Scripts/GenerateJob.cs
+++ Assets/Scripts/GenerateJob.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class GenerateJob : MonoBehaviour
 {
     public GameObject part;
+    public GameObject counter;
+
+    private GameObject partInstance;
+    private GameObject counterInstance;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(part, new Vector3(1 , 1, 0), Quaternion.identity);
+        partInstance = Instantiate(part, new Vector3(1 , 1, 0), Quaternion.identity);
+        counterInstance = Instantiate(counter, new Vector3(1,1,0), Quaternion.identity);
+        counterInstance.GetComponent<CounterBehaviour>().AttachJob(this);
+    }
+
+    public void removePart() {
+        Destroy(partInstance);
     }
 
     // Update is called once per frame
