1f5245f356e22bddd58e17a20f45a6d31c4ad7ef
Yashu7 <janmalawski2@gmail.com>
2020-02-01 19:53:57 +0100

    Clock

--- Assets/Scripts/SlotBehaviour.cs
+++ Assets/Scripts/SlotBehaviour.cs
@@ -9,7 +9,7 @@ public class SlotBehaviour : MonoBehaviour
     public List<Sprite> partSprite = new List<Sprite>();
     public List<Sprite> damagedPartSprite = new List<Sprite>();
     public GameObject partPrefab;
-    public GameObject counter; 
+    public GameObject counter;
     public GameObject resultDisplay;
     public int partsCounter = 3;
     const float partOffset = 1f;
@@ -19,71 +19,79 @@ public class SlotBehaviour : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
-    public void Repopulate() {
+    public void Repopulate()
+    {
         Debug.Log("Repopulating slot");
 
         float parentX = gameObject.transform.position.x;
         float parentY = gameObject.transform.position.y + 1;
         //Helmet
-        AddPart(parentX, parentY+0.1F, true);
+        AddPart(parentX, parentY + 0.1F, true);
         //Left Hand
-        AddPart(parentX-partOffset-0.1F, parentY-partOffset);
+        AddPart(parentX - partOffset - 0.1F, parentY - partOffset);
         //Right Hand
-        AddPart(parentX+partOffset+0.1F, parentY-partOffset, true);
+        AddPart(parentX + partOffset + 0.1F, parentY - partOffset, true);
         //Breast Plate
-        AddPart(parentX, parentY-(partOffset + 0.25F));
+        AddPart(parentX, parentY - (partOffset + 0.25F));
         //Left Leg
-        AddPart(parentX-partOffset+0.5F, parentY-(partOffset*2.4F));
+        AddPart(parentX - partOffset + 0.5F, parentY - (partOffset * 2.4F));
         //Right Leg
-        AddPart(parentX+partOffset-0.5F, parentY-(partOffset*2.4F));
+        AddPart(parentX + partOffset - 0.5F, parentY - (partOffset * 2.4F));
 
 
         counter.GetComponent<CounterBehaviour>().Restart();
         resultDisplay.GetComponent<Text>().text = "";
     }
 
-    void AddPart(float x, float y, bool isBroken= false) 
+    void AddPart(float x, float y, bool isBroken = false)
     {
-            Debug.Log("Creating new part in slot");
-            GameObject part = Instantiate(partPrefab, new Vector3(
-                x, 
-                y, 
-                0
-                )
-                , Quaternion.identity);
-       
-            s = partSprite[parts.Count];
-       
+        Debug.Log("Creating new part in slot");
+        GameObject part = Instantiate(partPrefab, new Vector3(
+            x,
+            y,
+            0
+            )
+            , Quaternion.identity);
+
+        s = partSprite[parts.Count];
+
         part.GetComponent<SpriteRenderer>().sprite = s;
         parts.Add(part);
-        
+
         part.GetComponent<PartBehaviour>().SetParrentSlot(this);
-            if(!isBroken) {
-                part.GetComponent<PartBehaviour>().Fix();
+        if (!isBroken)
+        {
+            part.GetComponent<PartBehaviour>().Fix();
             part.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0.5F);
-        } else {
-                part.GetComponent<PartBehaviour>().Break();
-            }
+        }
+        else
+        {
+            part.GetComponent<PartBehaviour>().Break();
+        }
     }
 
-    public void Success() 
+    public void Success()
     {
         resultDisplay.GetComponent<Text>().text = "Job Done!";
-        Clear();
+        
+        Invoke("Clear",0.5f);
     }
 
-    public void Fail() 
+    public void Fail()
     {
         resultDisplay.GetComponent<Text>().text = "Fail!";
         Clear();
     }
 
-    public void Clear() {
+    public void Clear()
+    {
         Debug.Log("Clearing parts");
-        foreach(GameObject part in parts) {
+        
+        foreach (GameObject part in parts)
+        {
             Destroy(part);
         }
         parts.Clear();
@@ -93,19 +101,25 @@ public class SlotBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(parts.Count < 1) {
+        if (parts.Count < 1)
+        {
             return;
         }
         toFix = 0;
-        foreach(GameObject part in parts) {
-            if(!part.GetComponent<PartBehaviour>().IsFixed()) {
+        foreach (GameObject part in parts)
+        {
+            if (!part.GetComponent<PartBehaviour>().IsFixed())
+            {
                 toFix++;
             }
         }
         //Debug.Log("parts to fix:" + toFix.ToString());
-        if(toFix < 1) {
+        if (toFix < 1)
+        {
             Debug.Log("Job done!");
-           Success();
+            
+            Success();
         }
     }
+    
 }
