64c65021cbe837a1d0c3c118418da66b3ab1848f
mmalawski <mieszkomalawski@cdprojektred.com>
2020-02-01 12:40:59 +0100

    restart

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartGame : MonoBehaviour
+{
+    public Button restart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restart = gameObject.GetComponent<Button>();
+        restart.onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked() 
+    {
+        Debug.Log("Restart game");
+            SceneManager.LoadScene (sceneName:"SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
