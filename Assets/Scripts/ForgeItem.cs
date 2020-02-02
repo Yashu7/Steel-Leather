e17343acb11cbc322a70df3275496a2feb0ba659
Yashu7 <janmalawski2@gmail.com>
2020-02-02 11:08:19 +0100

    Not enough resources

--- Assets/Scripts/ForgeItem.cs
+++ Assets/Scripts/ForgeItem.cs
@@ -28,7 +28,7 @@ public class ForgeItem : MonoBehaviour
     }
     void ButtonClicked()
     {
-       
+
         if (GameObject.FindWithTag("Clicked"))
         {
 
@@ -40,25 +40,47 @@ public class ForgeItem : MonoBehaviour
             if (gameGenerator.GetComponent<PlayerInventory>().CanIForge(currentPart.GetComponent<PartBehaviour>().ReturnLeather(), currentPart.GetComponent<PartBehaviour>().ReturnSteel()))
             {
                 gameGenerator.GetComponent<PlayerInventory>().DeduceLeatherCost(currentPart.GetComponent<PartBehaviour>().ReturnLeather());
+                
                 gameGenerator.GetComponent<PlayerInventory>().DeduceSteelCost(currentPart.GetComponent<PartBehaviour>().ReturnSteel());
-
+                
                 //fix part - notify slot
                 currentPart.GetComponent<PartBehaviour>().Fix();
 
                 PlayHit();
                 currentPart.tag = "Job";
             }
-            
+            if(!gameGenerator.GetComponent<PlayerInventory>().CanIForge(currentPart.GetComponent<PartBehaviour>().ReturnLeather(),0))
+                {
+                StartCoroutine(notEnoughLeather());
+            }
+            if (!gameGenerator.GetComponent<PlayerInventory>().CanIForge(0, currentPart.GetComponent<PartBehaviour>().ReturnSteel()))
+            {
+                StartCoroutine(notEnoughSteel());
+            }
+
         }
-
-        
-
-
-
-
     }
 
-     private void PlayHit()
+     public IEnumerator notEnoughLeather()
+        {
+        GameObject.Find("AmountOfLeather").GetComponent<Text>().color = Color.red;
+        yield return new WaitForSeconds(0.5F);
+        GameObject.Find("AmountOfLeather").GetComponent<Text>().color = Color.green;
+    }
+    public IEnumerator notEnoughSteel()
+    {
+        GameObject.Find("AmountOfSteel").GetComponent<Text>().color = Color.red;
+        yield return new WaitForSeconds(0.5F);
+        GameObject.Find("AmountOfSteel").GetComponent<Text>().color = Color.green;
+    }
+
+
+
+
+
+
+
+    private void PlayHit()
     {
         Debug.Log("playing sound");
         anvilHitSoundSource = GetComponent<AudioSource>();
