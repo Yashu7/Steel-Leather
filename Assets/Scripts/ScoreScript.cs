4addb95d13c9b0c923721d601d431e6f4eeaf319
Yashu7 <janmalawski2@gmail.com>
2020-02-02 14:17:48 +0100

    Global Score

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public int s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("MyScore"))
+        {
+            s = GameObject.Find("MyScore").GetComponent<ScoreBehaviour>().GetScore();
+        }
+
+       
+        if(GameObject.Find("FinalScore"))
+        {
+            GameObject.Find("FinalScore").GetComponent<Text>().text = s.ToString();
+        }
+    }
+}
