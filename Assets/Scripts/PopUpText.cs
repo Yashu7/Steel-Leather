8866a494d6874bd3df039d0ad48a4e8c26a67943
JanMalawski <janmalawski2@gmail.com>
2020-02-05 17:13:05 +0100

    Fade Out animation

--- Assets/Scripts/PopUpText.cs
+++ Assets/Scripts/PopUpText.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class PopUpText : MonoBehaviour
 {
+    public Vector2 startPosition;
+    
+    public Color cr;
     // Start is called before the first frame update
     void Start()
     {
-
+        startPosition = gameObject.transform.position;
+        cr = gameObject.GetComponent<Text>().color;
+        
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +22,9 @@ public class PopUpText : MonoBehaviour
     }
     public void popUpMessage(string message1, int message2)
     {
+        
         gameObject.GetComponent<Text>().text = message1 + message2.ToString();
+        StartCoroutine(fadeOut());
         StartCoroutine(FadeOutRoutine());
 
     }
@@ -26,5 +32,16 @@ public class PopUpText : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         gameObject.GetComponent<Text>().text = "";
+        gameObject.transform.position = startPosition;
+    }
+    private IEnumerator fadeOut()
+    {
+        for (float i = 0; i < 0.1F;)
+        {
+            gameObject.GetComponent<Text>().color -= new Color32(0, 0, 0, 23);
+            gameObject.transform.position = gameObject.transform.position + new Vector3(0, i, 0);
+            i = i + 0.01F;
+            yield return null;
+        }
     }
 }
