d85c5f104bf97ce0d94d5c627991df5c6d9071ef
Yashu7 <janmalawski2@gmail.com>
2020-02-10 02:32:09 +0100

    GameOver Scene(Android version script)

---
+++ Assets/isAndroid.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isAndroid : MonoBehaviour
+{
+    public bool isBuildAndroid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isBuildAndroid)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
